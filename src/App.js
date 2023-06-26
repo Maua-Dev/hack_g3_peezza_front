e858ff80feeac61864fd5c6a63af1d4eb92eb019
Vitor Soller <vgsoller@gmail.com>
2023-06-26 19:18:00 -0300

    change color

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <h1 className="Title">Bem-vindo ao seu aplicativo! 🎃</h1>
+      <h1 className="Title">Meu aplicativo! 🎃</h1>
     </div>
   );
 }
