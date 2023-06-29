6b2218730a37e4b1d6528e61a896402b5f222401
Felipe Carillo <felipecarillo@outlook.com>
2023-06-28 21:32:22 -0300

    Update no css do Bottom

--- src/front/Home/Bottom/Bottom.js
+++ src/front/Home/Bottom/Bottom.js
@@ -1,6 +1,6 @@
 import React from "react";
 import "./Bottom.css";
-import { useState, useEffect } from "react";
+import { useState } from "react";
 import { Link } from 'react-router-dom';
 import { ShowCarrinho } from './Utils';
 
