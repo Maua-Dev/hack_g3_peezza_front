61416e74d55d3c438cd230857ac38eda057ac712
Felipe Carillo <felipecarillo@outlook.com>
2023-07-07 20:57:22 -0300

    Login Interface for administration.

--- src/admin_interface/front/Home/Login.js
+++ src/admin_interface/front/Home/Login.js
@@ -1,8 +1,28 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './Login.css';
 import { BsFillEyeFill, BsFillEyeSlashFill } from "react-icons/bs";
+import { fetchData } from "./data/repo_mock";
 
 export default function Login() {
+
+  const [showPassword, setClickedShow] = useState(false);
+
+  const handleShowPassword = () => {
+    setClickedShow(!showPassword)
+    var x = document.getElementById('password')
+    if (x.type === 'password'){
+      x.type = "text";
+    } else {
+      x.type = "password";
+    }
+  };
+
+  const handleLogin = (username, password) => {
+    const accounts = fetchData();
+    const existingUser = accounts.find((account) => account.username === username && account.password === password) ? (true) : (false);
+    console.log(existingUser);
+  };
+
   return(
     <div className='Login'>
       <div className='LoginBox'>
@@ -14,21 +34,22 @@ export default function Login() {
           </div>
         </div>
         <div className='MidBox'>
-          <div className='Input'>
+          <div className='Input1'>
             <label>Usuario:</label>
-            <input></input>
+            <input type='text' id='username'></input>
           </div>
-          <div className='Input'>
+          <div className='Input2'>
             <label>Senha:</label>
-            <input></input>
-            <BsFillEyeFill className='showPassword' size={'70px'}></BsFillEyeFill>
-            <div className='senhaOptions'>
-              <link></link>
+            <div className='PasswordBox'>
+              <input type='password' id='password'></input>
+              {showPassword ? 
+              (<BsFillEyeSlashFill onClick={handleShowPassword} className='showPassword' size={'60px'} color='rgb(209, 209, 209)' />) :
+              (<BsFillEyeFill onClick={handleShowPassword} className='showPassword' size={'60px'} color='rgb(209, 209, 209)' />)}
             </div>
           </div>
         </div>
         <div className='BottomBox'>
-          <button>Acessar</button>
+          <button onClick={() => handleLogin(document.getElementById('username').value, document.getElementById('password').value)}>LOGIN</button>
         </div>
       </div>
     </div>
--- src/admin_interface/front/Home/a.py
+++
@@ -1,5 +0,0 @@
-a = input('A: ').split(", ")
-
-for index, ele in enumerate(a):
-    a[index]=ele+";"
-    print(a[index])
