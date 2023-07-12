25ee0f33f986a7ffc5c29c41842f993eb928b1f6
GabrielBianconiconi <gabrielbianconiconi@gmail.com>
2023-07-12 16:00:02 -0300

    Update on retirada page🍕

--- src/cliente_interface/front/Retirada/MidSection/MidRetirada.js
+++ src/cliente_interface/front/Retirada/MidSection/MidRetirada.js
@@ -5,15 +5,20 @@ import gifImage from './Loading.gif';
 
 const nome = localStorage.getItem('nome');
 export const Mid = () => {
-    return (
-      <div className="mid_p">
-         <BackToPayment /> 
-         <div className="pedido">
-          <h1>{nome} seu pedido foi enviado:</h1>
-          </div>
-            <div>
-              <img src={gifImage} alt="Loading..." />
-            </div>
+  return (
+    <div className="mid_p">
+      <BackToPayment /> 
+      <div className="pedido">
+        <h1>{nome} seu pedido foi enviado:</h1>
       </div>
-    );
+      <div>
+        <img src={gifImage} alt="Loading..." />
+        <div className="status">
+          <span>Status:</span>
+        </div>
+        <div className="line"></div>
+          <span className="centered">[Status]</span>       
+      </div>
+    </div>
+  );
 }
