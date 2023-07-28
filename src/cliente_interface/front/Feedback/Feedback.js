d893c8c2402a9792e048b7c9cc173db3edaa0f85
Gabriel Bianconi <gabrielbianconiconi@gmail.com>
2023-07-28 09:44:28 -0300

    created BottomFeed

--- src/cliente_interface/front/Feedback/Feedback.js
+++ src/cliente_interface/front/Feedback/Feedback.js
@@ -2,6 +2,7 @@ import React from 'react';
 import './Feedback.css';
 import { Top } from './Top/Top';
 import { MidFeed } from './Mid/MidFeed';
+import { BottomFeed } from './Bottom/BottomFeed';
 
 
 export default function Feedback() {
@@ -9,6 +10,7 @@ export default function Feedback() {
       <div className='Feedback'>
         <Top></Top>
         <MidFeed></MidFeed>
+        <BottomFeed></BottomFeed>
       </div>
     );
 }
