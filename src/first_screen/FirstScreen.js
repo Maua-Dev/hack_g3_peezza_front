b71a6b8397e6464dbf8a1512f061a2c668e15dee
Felipe Carillo <felipecarillo@outlook.com>
2023-08-04 23:39:09 -0300

    Update in first_screen

--- src/first_screen/FirstScreen.js
+++ src/first_screen/FirstScreen.js
@@ -15,10 +15,10 @@ export default function FirstScreen() {
   return (
     <div className="FirstScreen">
       <Link to='/admin' className="Box" id="admin">
-        <div className="BoxTitle">Estabelecimento</div>
+        <div className="BoxTitle">Estabelecimento (Desktop)</div>
       </Link>
       <Link to='/client'className="Box" id="client">
-        <div className="BoxTitle">Cliente</div>
+        <div className="BoxTitle">Cliente (Mobile)</div>
       </Link>
     </div>
   );
